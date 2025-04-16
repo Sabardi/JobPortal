3608bcea346ff5d2f8f8ccd53794a13c6ba6dbfb
Sabardi <Sabardibahari@gmail.com>
2025-04-16 18:10:27 +0700

    Fix foreign key constraint in bookmarks migration and restore job_vacancy_categories migration

--- database/migrations/2025_04_16_005734_create_bookmarks_table.php
+++ database/migrations/2025_04_16_005734_create_bookmarks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('job_vacancies_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('job_vacancies_id')->constrained('job_vacancies')->onDelete('cascade');
             $table->timestamps();
         });
     }
