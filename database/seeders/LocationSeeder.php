ff779666970fb44c495c7fbbb73f926bbf0ffae0
Sabardi <Sabardibahari@gmail.com>
2025-04-16 12:56:08 +0700

    Add Location model, migration, and seeder for location data

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Location;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LocationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $locations = [
+            'Kota Mataram',
+            'Kabupaten Lombok Barat',
+            'Kabupaten Lombok Timur',
+            'Kabupaten Lombok Tengah',
+            'Kabupaten Sumbawa',
+            'Kabupaten Sumbawa Barat',
+            'Kabupaten Sumbawa Selatan',
+            'Kabupaten Dompu',
+        ];
+
+        foreach ($locations as $location) {
+            Location::updateOrCreate(
+                ['name' => $location], // Kolom yang menjadi acuan pencarian
+                // ['name' => $location]  // Data yang ingin diupdate atau dibuat
+            );
+        }
+    }
+}
